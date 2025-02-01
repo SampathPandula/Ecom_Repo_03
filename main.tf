4241db14ba0f686bbec8ace37276c603c73dac23
sampath <sampathsams319@gmail.com>
2025-02-01 12:40:37 +0530

    commit_01

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "ap-south-2"
+}
+
+resource "aws_vpc" "ecommerce_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.ecommerce_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.ecommerce_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-2b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_eks_cluster" "EKS_ECom_Cluster" {
+  name     = "ecommerce-cluster"
+  role_arn = "arn:aws:iam::713881815267:role/EKS-Cluster-Role"
+
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]  # Use the correct subnet references
+  }
+}
